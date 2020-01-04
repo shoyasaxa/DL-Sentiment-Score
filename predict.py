fc0fb6f60fa4422062dea827649d7453871d9a7e
Shoya Yoshida <sy519@georgetown.edu>
2020-01-04 17:40:56 -0500

    update

--- predict.py
+++ predict.py
@@ -86,5 +86,13 @@ def predict(path):
 	data_sample_6 = "If you don't like complicated equations, then I wouldn't recommend it."
 	print(predict_score(loaded_model, data_sample_6, word_idx))
 
+	data_sample_6 = "Definitely not the best at explaining concepts, which is kind of critical considering that this course is very abstract and theoretical. It can also get very hard to take notes on his lecture because he often would not write down a complex idea he is talking about and/or scribble very few words about it. He does regurgitate the textbook almost verbatim, so you can totally get away with not attending class."
+    print(live_test(loaded_model, data_sample_6, word_idx))
+
+    data_sample_6 = "This is the worst CS class I've taken at Columbia by far. Expect extremely large projects that have nothing to do with what is covered in lecture, and a fucked up grading system that doesn't reflect how well you understand the material or how hard you work. Oh and by the way, unless you pass 100% of test cases, you get a ZERO on the project. You only start to earn points through optimization of your circuit. I got the highest possible score on the first 5 projects, and got a poor score (I think around a 30% or something) on the last project, and ended up with a B in the class. I'm honestly not sure how it's possible to get a good grade in the class. Moral of the story: RUN AT ALL COSTS. Rubenstein may have poor reviews but there is no possible way his class could be worse than this. Martha Kim ruined my semester and I have no respect for her - classes like this are why Columbia has a mental health problem."
+    print(live_test(loaded_model, data_sample_6, word_idx))
+
+       
+
 if __name__ == "__main__":
 	predict(sys.argv[1])
