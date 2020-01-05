a3add0a95cf01aff0fee282247844950991d9aeb
Shoya Yoshida <sy519@georgetown.edu>
2020-01-04 20:45:04 -0500

    update

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ from keras.layers import Dense, Flatten, LSTM, Bidirectional, Dropout
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 import h5py
+from kerastuner.tuners import RandomSearch
 
 from nltk.tokenize import RegexpTokenizer
 
@@ -40,14 +41,28 @@ def load_all_data(data_dir,prediction_path, glove_file, first_run):
 
 	return train_x, train_y, test_x, test_y, val_x, val_y, weight_matrix, word_idx, max_seq_length
 
-def create_model_rnn(weight_matrix, max_words, EMBEDDING_DIM):
+def create_model_rnn(hp, weight_matrix, max_words, EMBEDDING_DIM):
 
 	# create the model
 	model = Sequential()
 	model.add(Embedding(len(weight_matrix), EMBEDDING_DIM, weights=[weight_matrix], input_length=max_words, trainable=False))
-	model.add(Bidirectional(LSTM(128, dropout=0.2, recurrent_dropout=0.2)))
-	model.add(Dense(512, activation='relu'))
-	model.add(Dropout(0.50))
+	
+	for i in range(hp.Int('num_layers_lstm',1,4)):
+		model.add(Bidirectional(LSTM(
+			units=hp.Int('units_lstm',min_value=64,max_value=1024,step=64), 
+			dropout=hp.Float('lstm_dropout',min_value=0,max_value=0.5,step=0.1), 
+			recurrent_dropout=hp.Float('lstm_recurr_dropout',min_value=0,max_value=0.5,step=0.1)
+		)))
+
+	for i in range(hp.Int('num_layers_dense',1,6)):
+		model.add(Dense(
+			units=hp.Int('units_dense'min_value=256,max_value=2048,step=128), 
+			activation='relu'
+		))
+		model.add(Dropout(
+			rate=hp.Float('dense_dropout',min_value=0,max_value=0.5,step=0.1)
+		))
+
 	model.add(Dense(10, activation='softmax'))
 	# try using different optimizers and different optimizer configs
 	model.compile(loss='categorical_crossentropy',optimizer='adam', metrics=['accuracy'])
@@ -73,16 +88,21 @@ def train(root_path):
 	saveBestModel = keras.callbacks.ModelCheckpoint(root_path+'/model/best_model.hdf5', monitor='val_acc', verbose=0, save_best_only=True, save_weights_only=False, mode='auto', period=1)
 	earlyStopping = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=3, verbose=0, mode='auto')
 
+
+	tuner = RandomSearch(
+
+	)
+
 	# Fit the model
-	model.fit(train_x, train_y, batch_size=BATCH_SIZE, epochs=25,validation_data=(val_x, val_y), callbacks=[saveBestModel, earlyStopping])
-	# Final evaluation of the model
-	score, acc = model.evaluate(test_x, test_y, batch_size=BATCH_SIZE)
+	# model.fit(train_x, train_y, batch_size=BATCH_SIZE, epochs=25,validation_data=(val_x, val_y), callbacks=[saveBestModel, earlyStopping])
+	# # Final evaluation of the model
+	# score, acc = model.evaluate(test_x, test_y, batch_size=BATCH_SIZE)
 
-	print('Test score:', score)
-	print('Test accuracy:', acc)
+	# print('Test score:', score)
+	# print('Test accuracy:', acc)
 
-	model.save_weights(root_path+"/model/best_model.h5")
-	print("Saved model to disk")
+	# model.save_weights(root_path+"/model/best_model.h5")
+	# print("Saved model to disk")
 
 if __name__ == "__main__":
 	train(sys.argv[1])
