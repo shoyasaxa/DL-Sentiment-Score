d32dbbb23fe39d2aa23750114a7361028854746c
Shoya Yoshida <sy519@georgetown.edu>
2020-01-07 20:42:44 -0500

    fix

--- predict.py
+++ predict.py
@@ -213,7 +213,7 @@ def predict_review_score_v2(path,data_path):
 	unscaled_scores = [] 
 	print("calculating scores...")
 	for pred in pred_list:
-		top_3_index = np.argsort(pred)[-3:]
+		top_3_index = np.array(np.argsort(pred)[-3:])
 		top_3_scores = pred[top_3_index]
 		top_3_weights = top_3_scores/np.sum(top_3_scores)
 		unscaled_score = np.round(np.dot(top_3_index, top_3_weights)/10, decimals = 2)
