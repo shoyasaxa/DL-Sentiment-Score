1514e6b87f3cb019bf28a39ec5b575c73fe15d53
Shoya Yoshida <sy519@georgetown.edu>
2020-01-07 20:26:54 -0500

    make predict more efficient

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@ import numpy as np
 import sys
 import h5py
 from keras.models import load_model,model_from_json
+from keras.preprocessing.sequence import pad_sequences
 from nltk.tokenize import RegexpTokenizer
 import pandas as pd 
 import numpy as np 
@@ -12,51 +13,52 @@ from sklearn.preprocessing import MinMaxScaler
 
 import re
 
-alphabets= "([A-Za-z])"
-prefixes = "(Mr|St|Mrs|Ms|Dr|Prof|etc)[.]"
-suffixes = "(Inc|Ltd|Jr|Sr|Co)"
-starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
-acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
-websites = "[.](com|net|org|io|gov)"
-
 def split_into_sentences(text):
-    text = " " + text + "  "
-    text = text.replace("\n"," ")
-    text = re.sub(prefixes,"\\1<prd>",text)
-    text = re.sub(websites,"<prd>\\1",text)
-    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
-    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
-    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
-    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
-    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
-    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
-    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
-    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
-    if "”" in text: text = text.replace(".”","”.")
-    if "\"" in text: text = text.replace(".\"","\".")
-    if "!" in text: text = text.replace("!\"","\"!")
-    if "?" in text: text = text.replace("?\"","\"?")
-    text = text.replace(".",".<stop>")
-    text = text.replace("?","?<stop>")
-    text = text.replace("!","!<stop>")
-    text = text.replace("<prd>",".")
-    sentences = text.split("<stop>")
-    sentences = sentences[:-1]
-    sentences = [s.strip() for s in sentences]
-    return sentences
+	alphabets= "([A-Za-z])"
+	prefixes = "(Mr|St|Mrs|Ms|Dr|Prof|etc)[.]"
+	suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+	starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+	acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+	websites = "[.](com|net|org|io|gov)"
+
+	text = " " + text + "  "
+	text = text.replace("\n"," ")
+	text = re.sub(prefixes,"\\1<prd>",text)
+	text = re.sub(websites,"<prd>\\1",text)
+	if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+	text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
+	text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+	text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+	text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
+	text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+	text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+	text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
+	if "”" in text: text = text.replace(".”","”.")
+	if "\"" in text: text = text.replace(".\"","\".")
+	if "!" in text: text = text.replace("!\"","\"!")
+	if "?" in text: text = text.replace("?\"","\"?")
+	text = text.replace(".",".<stop>")
+	text = text.replace("?","?<stop>")
+	text = text.replace("!","!<stop>")
+	text = text.replace("<prd>",".")
+	sentences = text.split("<stop>")
+	sentences = sentences[:-1]
+	sentences = [s.strip() for s in sentences]
+	return sentences
 
 def predict_score(trained_model, sentence, word_idx):
 	# print(sentence)
-	sentence =  sentence.replace("\\","")
 	sentence_list = []
 	sentence_list_np = np.zeros((56,1))
 	# split the sentence into its words and remove any punctuations.
 	tokenizer = RegexpTokenizer(r'\w+')
 	data_sample_list = tokenizer.tokenize(sentence)
-
-	labels = np.array([1,2,3,4,5,6,7,8,9,10], dtype = "int")
 	#word_idx['I']
 	# get index for the live stage
+
+	if (len(data_sample_list) > 56):
+		data_sample_list = data_sample_list[:56]
+
 	data_index = np.array([word_idx[word.lower()] if word.lower() in word_idx else 0 for word in data_sample_list])
 	data_index_np = np.array(data_index)
 	# print(data_index_np)
@@ -67,11 +69,8 @@ def predict_score(trained_model, sentence, word_idx):
 	data_index_np_pad = padded_array.astype(int)
 	sentence_list.append(data_index_np_pad)
 
-	if (len(sentence_list) > 56):
-		sentence_list_np = np.asarray(sentence_list[:56])
-	else:
-		sentence_list_np = np.asarray(sentence_list)
-
+	sentence_list_np = np.asarray(sentence_list[:56])
+	
 	# get score from the model
 	score = trained_model.predict(sentence_list_np, batch_size=1, verbose=0)
 	#print (score)
@@ -143,11 +142,96 @@ def test_predict(path):
 
 	for sentence in sentences:
 		print(predict_score(loaded_model,sentence,word_idx))
+		print(sentence)
 
 		# for i, model in enumerate(models):
 		# 	print("model {}:".format(i))
 		# 	print(predict_score(model,sentence,word_idx))
-   
+
+def predict_review_score_v2(path,data_path):
+	glove_file = path+'/Data/glove/glove_6B_100d.txt'
+	weight_matrix, word_idx = uf.load_embeddings(glove_file)
+	weight_path = path +'/model/best_model.hdf5'
+	print("loading model...)")
+	loaded_model = load_model(weight_path)
+
+	df = pd.read_excel(data_path)
+	df = df.loc[df["is_workload"] == 0].copy()
+
+	df['review'] = df['review'].replace('\n','. ', regex=True)
+	df['review'] = df['review'].replace("'\\\\'","", regex=True)
+
+	reviews = df["review"].values.tolist()
+	prof_ids = df["prof_id_culpa"].values.tolist() 
+	review_ids = df["id_culpa"].values.tolist()
+	# TODO: Include num_agrees to weight reviews 
+
+	tokenizer = RegexpTokenizer(r'\w+')
+	sentence_level_data = []
+	print("starting to make df_sentence...")
+	print("There are {} reviews".format(len(reviews)))
+	for i, review in enumerate(reviews): 
+		if (i%2000==0):
+			print(i)
+		sentences = split_into_sentences(review)
+		for sentence in sentences: 
+			sentence_tokenized = tokenizer.tokenize(sentence)
+			sentence_indexed = np.array([word_idx[word.lower()] if word.lower() in word_idx else 0 for word in sentence_tokenized])
+			# if (len(sentence_indexed)>0):
+			sentence_level_data.append((prof_ids[i],review_ids[i],sentence,sentence_indexed))
+
+	df_sentence = pd.DataFrame(sentence_level_data, columns=["prof_id","review_id","sentence","sentence_indexed"])
+	
+	pd.set_option('display.width', 1000) 
+	pd.set_option('display.max_columns', 1000) 
+	print(df_sentence.head(10))
+
+	padded = pad_sequences(df_sentence["sentence_indexed"].values,maxlen=56,padding='post',truncating='post')
+	print(padded)
+	print(type(padded))
+	print(padded[0])
+
+	# try:
+	# 	print(padded.shape)
+	# 	print(df_sentence["sentence_indexed"].shape)
+	# 	df_sentence["sentence_indexed_padded"] = padded[0]
+	# except Exception as e:
+	# 	print(e)
+	
+	# try:
+	# 	d = pd.DataFrame(padded)
+	# 	print(d)
+	# except Exception as e:
+	# 	print(e)
+
+	print("predicting...")
+	pred = loaded_model.predict(padded,batch_size=1024,verbose=2)
+	print(pred)
+	# df_sentence["preds"] = pred
+
+	pred_list = pred.tolist()
+	unscaled_scores = [] 
+	print("calculating scores...")
+	for pred in pred_list:
+		top_3_index = np.argsort(pred)[0][-3:]
+		top_3_scores = pred[0][top_3_index]
+		top_3_weights = top_3_scores/np.sum(top_3_scores)
+		unscaled_score = np.round(np.dot(top_3_index, top_3_weights)/10, decimals = 2)
+		unscaled_scores.append(unscaled_score)
+
+	df_sentence["score"] = unscaled_scores
+
+	new_range = 4 
+	new_min = 1 
+	old_range = 1
+	df_sentence["score"] = round(((df_sentence["score"] * new_range)/old_range)+new_min,2)
+
+	minmax_scaler = MinMaxScaler()
+	df_sentence["score_min_max_scaled"] = minmax_scaler.fit_transform(df_sentence["score"].values)
+
+	df_sentence[["prof_id","review_id","sentence",'score','score_min_max_scaled']].to_excel("sentence_level_review_and_scores.xlsx")
+
+
 def predict_review_score(path,data_path):
 	glove_file = path+'/Data/glove/glove_6B_100d.txt'
 	weight_matrix, word_idx = uf.load_embeddings(glove_file)
@@ -158,10 +242,14 @@ def predict_review_score(path,data_path):
 	df = df.loc[df["is_workload"] == 0].copy()
 
 	df['review'] = df['review'].replace('\n','. ', regex=True)
+	df['review'] = df['review'].replace("\\","", regex=True)
+
+
 	reviews = df["review"].values.tolist()
 	is_workload_list = df["is_workload"].values.tolist()
 	prof_ids = df["prof_id_culpa"].values.tolist() 
 	review_ids = df["id_culpa"].values.tolist()
+
 
 	review_scores = [] 
 	for i, review in enumerate(reviews):
@@ -178,8 +266,8 @@ def predict_review_score(path,data_path):
 				score_sum += score 
 		review_scores.append(score_sum/len(sentences))
 
-		if (i%1000==0):
-			print(i)
+		if (i%100==0):
+			print("{}/{}".format(i,len(reviews)))
 
 	df["scores"] = review_scores
 
@@ -190,7 +278,7 @@ def predict_review_score(path,data_path):
 	df["scores"] = round(((df["scores"] * new_range)/old_range)+new_min,2)
 
 	minmax_scaler = MinMaxScaler()
-	df["scores_min_max_scaled"] = minmax_scaler.fit_transform(df["scores_min_max_scaled"].values)
+	df["scores_min_max_scaled"] = minmax_scaler.fit_transform(df["scores"].values)
 
 	df[['prof_id_culpa','id_culpa','review','scores','scores_min_max_scaled']].to_excel("/output/dl_review_scores_v1.xlsx")
 
@@ -198,6 +286,9 @@ def predict_review_score(path,data_path):
 	df_prof_scores.to_excel('/output/dl_prof_scores_v1.xlsx')
 
 
+
+
 if __name__ == "__main__":
-	test_predict(sys.argv[1])
-	predict_review_score(sys.argv[1],sys.argv[2])
+	# test_predict(sys.argv[1])
+	# predict_review_score(sys.argv[1],sys.argv[2])
+	predict_review_score_v2(sys.argv[1],sys.argv[2])
